9b40896ee7c73439aaa9e9a891803262c60b21c0
Ubaid <ubaidmohammad2007@gmail.com>
2026-04-01 21:10:58 +0530

    UC4: Display Hello with multiple command-line arguments

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,13 +1,21 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        String name;
 
         if (args.length > 0) {
-            name = args[0];
-        } else {
-            name = "World";
-        }
+            System.out.print("Hello ");
 
-        System.out.println("Hello " + name);
+            for (int i = 0; i < args.length; i++) {
+                System.out.print(args[i]);
+
+                // Add space between names (except last)
+                if (i != args.length - 1) {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println(); // move to next line
+        } else {
+            System.out.println("Hello World");
+        }
     }
 }
