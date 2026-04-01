b2ffc15aa355943a165e4f253f3a7a269ef16d19
Ubaid <ubaidmohammad2007@gmail.com>
2026-04-01 21:22:51 +0530

    UC7: Use String.join() for multiple names

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,17 +4,8 @@ public class HelloWorld {
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-            String result = "";
-
-            // Enhanced for-loop
-            for (String name : args) {
-                result += name + " ";
-            }
-
-            // Remove last space using substring
-            result = result.substring(0, result.length() - 1);
-
-            System.out.println("Hello " + result);
+            String names = String.join(" ", args);
+            System.out.println("Hello " + names);
         }
     }
 }
